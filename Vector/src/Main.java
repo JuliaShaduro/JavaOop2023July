497d8f3812b704a1e4e4d09e249f93f58bcd7e6a
Julia <u.korjova@gmail.com>
2023-08-13 22:21:13 +0700

    Корректировка ошибок

--- Vector/src/Main.java
+++ Vector/src/Main.java
@@ -1,37 +1,41 @@
 import ru.academits.shaduro.vector.vector.Vector;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Vector vector1 = new Vector(1);
-        double[] array = {1.5, 3};
-        vector1.setArray(array);
-        System.out.println("Умножение вектора1 на скаляр" + Arrays.toString(vector1.getScalarProduct(-3)));
-        System.out.println("Разворот вектора1 = " + Arrays.toString(vector1.getVectorTurnaround()));
-        System.out.println("Длина вектора1 = " + vector1.getVectorLength());
-        System.out.println("Индекс компоненты = " + vector1.getIndex(2));
+        vector1.multiplyByScalar(-3);
+        System.out.println("Умножение вектора1 на скаляр = " + vector1);
+        vector1.unwrap();
+        System.out.println("Разворот вектора1 = " + vector1);
+        System.out.println("Длина вектора1 = " + vector1.getSum());
+        System.out.println("Индекс компоненты = " + vector1.getCoordinate(0));
         System.out.println("-----------------------------------------");
 
         Vector vector2 = new Vector(3.3, 35, 75);
-        System.out.println("Умножение вектора2 на скаляр" + Arrays.toString(vector2.getScalarProduct(-3)));
-        System.out.println("Разворот вектора2 = " + Arrays.toString(vector2.getVectorTurnaround()));
-        System.out.println("Длина вектора2 = " + vector2.getVectorLength());
-        System.out.println("Индекс компоненты = " + vector2.getIndex(2));
+        vector2.multiplyByScalar(6);
+        System.out.println("Умножение вектора2 на скаляр = " + vector2);
+        vector2.unwrap();
+        System.out.println("Разворот вектора2 = " + vector2);
+        System.out.println("Длина вектора2 = " + vector2.getSum());
+        System.out.println("Индекс компоненты = " + vector2.getCoordinate(2));
         System.out.println("-----------------------------------------");
 
         Vector vector3 = new Vector(3, 3.3, 17, 23);
-        System.out.println("Умножение вектора3 на скаляр" + Arrays.toString(vector3.getScalarProduct(-3)));
-        System.out.println("Разворот вектора3 = " + Arrays.toString(vector3.getVectorTurnaround()));
-        System.out.println("Длина вектора3 = " + vector3.getVectorLength());
-        System.out.println("Индекс компоненты = " + vector3.getIndex(2));
-        System.out.println("Установка компоненты вектора по индексу" + Arrays.toString(vector3.setCoordinates(2, 36)));
+        vector3.multiplyByScalar(12);
+        System.out.println("Умножение вектора3 на скаляр = " + vector3);
+        vector3.unwrap();
+        System.out.println("Разворот вектора3 = " + vector3);
+        System.out.println("Длина вектора3 = " + vector3.getSum());
+        System.out.println("Индекс компоненты = " + vector3.getCoordinate(2));
+        vector3.setCoordinate(2, 36);
+        System.out.println("Установка компоненты вектора по индексу" + vector3);
         System.out.println("-----------------------------------------");
 
-        Vector copy = new Vector(vector1);
+        Vector copy = new Vector(vector2);
+        System.out.println("Копия = " + copy);
 
-        System.out.println("Сложение двух векторов = " + Vector.getVectorsSum(vector1, vector2));
-        System.out.println("Вычитание векторов = " + Vector.getVectorsDifference(vector2, copy));
-        System.out.println("Скалярное произведение векторов = " + Vector.getVectorsScalarProduct(vector2, copy));
+        System.out.println("Сложение двух векторов = " + Vector.getResultSum(vector1, vector2));
+        System.out.println("Вычитание векторов = " + Vector.getResultDifference(vector3, copy));
+        System.out.println("Скалярное произведение векторов = " + Vector.getProductScalar(vector2, copy));
     }
 }
--- Vector/src/ru/academits/shaduro/vector/vector/Vector.java
+++ Vector/src/ru/academits/shaduro/vector/vector/Vector.java
@@ -3,38 +3,39 @@ package ru.academits.shaduro.vector.vector;
 import java.util.Arrays;
 
 public class Vector {
-    private double[] array;
+    private double[] coordinates;
 
-    public Vector(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("Количество координат <= 0 ");
+    public Vector(int coordinatesCount) {
+        if (coordinatesCount <= 0) {
+            throw new IllegalArgumentException("Количество координат <= 0, coordinatesCount = " + coordinatesCount);
         }
 
-        array = new double[n];
-    }
-
-    public void setArray(double[] array) {
-        this.array = array;
+        coordinates = new double[coordinatesCount];
     }
 
     public Vector(Vector vector) {
-        this.array = vector.array;
+        coordinates = Arrays.copyOf(vector.coordinates,
+                vector.coordinates.length);
     }
 
-    public Vector(double... array) {
-        this.array = array;
-    }
-
-    public Vector(int n, double... array) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("Количество координат <= 0 ");
+    public Vector(double... coordinates) {
+        if (coordinates.length == 0) {
+            throw new IllegalArgumentException("Количество координат = 0");
         }
 
-        this.array = Arrays.copyOf(array, n);
+        this.coordinates = Arrays.copyOf(coordinates, coordinates.length);
+    }
+
+    public Vector(int coordinatesCount, double... coordinates) {
+        if (coordinatesCount <= 0) {
+            throw new IllegalArgumentException("Количество координат <= 0, coordinatesCount = " + coordinatesCount);
+        }
+
+        this.coordinates = Arrays.copyOf(coordinates, coordinatesCount);
     }
 
     public int getSize() {
-        return array.length;
+        return coordinates.length;
     }
 
     @Override
@@ -42,79 +43,69 @@ public class Vector {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("{");
 
-        for (int i = 0; i < array.length; i++) {
-            stringBuilder.append(array[i]);
-
-            if (i + 1 < array.length) {
-                stringBuilder.append(",");
-            }
+        for (double coordinate : coordinates) {
+            stringBuilder.append(coordinate).append(", ");
         }
 
-        stringBuilder.append("}");
+        stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append('}');
 
-        return String.valueOf(stringBuilder);
+        return stringBuilder.toString();
     }
 
-    public void getVectorsSum(Vector vector) {
-        if (getSize() < vector.getSize()) {
-            this.array = Arrays.copyOf(array, vector.getSize());
+    public void add(Vector vector) {
+        if (coordinates.length < vector.coordinates.length) {
+            coordinates = Arrays.copyOf(coordinates, vector.coordinates.length);
         }
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] += i < vector.array.length ? vector.array[i] : 0;
+        for (int i = 0; i < coordinates.length; i++) {
+            coordinates[i] += vector.coordinates[i];
         }
     }
 
-    public void getVectorsDifference(Vector vector) {
-        if (getSize() < vector.getSize()) {
-            this.array = Arrays.copyOf(array, vector.getSize());
+    public void subtract(Vector vector) {
+        if (coordinates.length < vector.coordinates.length) {
+            coordinates = Arrays.copyOf(coordinates, vector.coordinates.length);
         }
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] -= i < vector.array.length ? vector.array[i] : 0;
+        for (int i = 0; i < coordinates.length; i++) {
+            coordinates[i] -= vector.coordinates[i];
         }
     }
 
-    public double[] getScalarProduct(double number) {
-        double[] scalarProduct = new double[getSize()];
-
-        for (int i = 0; i < scalarProduct.length; i++) {
-            scalarProduct[i] = array[i] * number;
+    public void multiplyByScalar(double number) {
+        for (int i = 0; i < coordinates.length; i++) {
+            coordinates[i] *= number;
         }
-
-        return scalarProduct;
     }
 
-    public double[] getVectorTurnaround() {
-        return getScalarProduct(-1);
+    public void unwrap() {
+        multiplyByScalar(-1);
     }
 
-    public double getVectorLength() {
-        int length = 0;
+    public double getSum() {
+        double length = 0;
 
-        for (double e : array) {
+        for (double e : coordinates) {
             length += e * e;
         }
 
         return Math.sqrt(length);
     }
 
-    public double getIndex(int index) {
-        if (index >= getSize() || index < 0) {
-            return -1;
+    public double getCoordinate(int index) {
+        if (index >= coordinates.length || index < 0) {
+            throw new IllegalArgumentException("Индекс за пределами массива, index = " + index);
         }
 
-        return array[index];
+        return coordinates[index];
     }
 
-    public double[] setCoordinates(int index, double coordinate) {
-        if (index >= getSize() || index < 0) {
-            return null;
+    public void setCoordinate(int index, double coordinate) {
+        if (index < 0 || index >= coordinates.length) {
+            throw new IllegalArgumentException("Индекс за пределами массива, index = " + index);
         }
 
-        array[index] = coordinate;
-
-        return array;
+        coordinates[index] = coordinate;
     }
 
     @Override
@@ -129,38 +120,40 @@ public class Vector {
 
         Vector vector = (Vector) o;
 
-        return Arrays.equals(array, vector.array) && (getSize() == vector.getSize());
+        return Arrays.equals(coordinates, vector.coordinates);
     }
 
     @Override
     public int hashCode() {
         final int prime = 37;
         int hash = 1;
-        hash = prime * hash + getSize();
-        hash = prime * hash + Arrays.hashCode(array);
+        hash = prime * hash + Arrays.hashCode(coordinates);
         return hash;
     }
 
-    public static Vector getVectorsSum(Vector vector1, Vector vector2) {
+    public static Vector getResultSum(Vector vector1, Vector vector2) {
         Vector vector = new Vector(vector1);
-        vector.getVectorsSum(vector2);
+        vector.add(vector2);
 
         return vector;
     }
 
-    public static Vector getVectorsDifference(Vector vector1, Vector vector2) {
+    public static Vector getResultDifference(Vector vector1, Vector vector2) {
         Vector vector = new Vector(vector1);
-        vector.getVectorsDifference(vector2);
+        vector.subtract(vector2);
 
         return vector;
     }
 
-    public static double getVectorsScalarProduct(Vector vector1, Vector vector2) {
-        double[] vectorsScalarProduct = new double[Math.max(vector1.getSize(), vector2.getSize())];
+    public static double getProductScalar(Vector vector1, Vector vector2) {
+        if (vector1.coordinates.length < vector2.coordinates.length) {
+            vector1.coordinates = Arrays.copyOf(vector1.coordinates, vector2.coordinates.length);
+        }
+
         double scalar = 0;
 
-        for (int i = 0; i < vectorsScalarProduct.length; i++) {
-            scalar += (i < vector1.array.length ? vector1.array[i] : 0) * (i < vector2.array.length ? vector2.array[i] : 0);
+        for (int i = 0; i < vector1.coordinates.length; i++) {
+            scalar += vector1.coordinates[i] * vector2.coordinates[i];
         }
 
         return scalar;
