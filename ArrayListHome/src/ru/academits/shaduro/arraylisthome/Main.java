c3c7e912629ce53d07680d666d4f8e032d9a90d7
Julia <u.korjova@gmail.com>
2023-11-02 16:13:16 +0700

    Корректировка имен методов и исключений.

--- ArrayListHome/src/ru/academits/shaduro/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academits/shaduro/arraylisthome/Main.java
@@ -1,41 +1,42 @@
 package ru.academits.shaduro.arraylisthome;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
-      //  System.out.println(lineArrayList(args[0]));
-
-        ArrayList<Integer> numbers = new ArrayList<>();
-
-        System.out.println(oddNumberArrayList(numbers));
-        System.out.println(noRepeatElementsArrayList(numbers));
-    }
-
-    public static ArrayList<Integer> noRepeatElementsArrayList (ArrayList<Integer> numbers) {
-        if (numbers.size() == 0) { //Todo добавила обработку искл. задала вопрос надо ли
-            throw new IndexOutOfBoundsException("Индекс = " + "}");
+        try {
+            ArrayList<String> linesList = getLinesList("input.");
+            System.out.println(linesList);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла.");
         }
 
-        ArrayList<Integer> noRepeatElements = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 6, 44, 7, 8, 9, 62));
 
-        for (Integer num : numbers) {
-            if (!noRepeatElements.contains(num)) {
-                noRepeatElements.add(num);
+        getOddNumbersList(numbers);
+        System.out.println(getNoRepeatNumbersList(numbers));
+    }
+
+    public static ArrayList<Integer> getNoRepeatNumbersList(ArrayList<Integer> numbers) {
+        ArrayList<Integer> noRepeatNumbers = new ArrayList<>(numbers.size());
+
+        for (Integer number : numbers) {
+            if (!noRepeatNumbers.contains(number)) {
+                noRepeatNumbers.add(number);
             }
         }
 
-        return noRepeatElements;
+        return noRepeatNumbers;
     }
 
-    public static ArrayList<Integer> oddNumberArrayList (ArrayList<Integer> numbers) {
-        if (numbers.size() == 0) {
-            throw new IndexOutOfBoundsException("Индекс = " + "}");
-        }
-
+    public static void getOddNumbersList(ArrayList<Integer> numbers) {
         for (int i = 0; i < numbers.size(); i++) {
             if (numbers.get(i) % 2 == 0) {
                 numbers.remove(i);
@@ -43,25 +44,19 @@ public class Main {
                 i--;
             }
         }
-
-        return numbers;
     }
 
-    public static ArrayList<String> lineArrayList (String inputFilePath) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFilePath))) {
-            ArrayList<String> names = new ArrayList<>();
+    public static ArrayList<String> getLinesList(String filePath) throws IOException {
+        ArrayList<String> linesList = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
             String line;
 
-            while ((line = bufferedReader.readLine()) != null) {
-                names.add(line);
+            while (null != (line = bufferedReader.readLine())) {
+                linesList.add(line);
             }
-
-            return names;
-
-        } catch (IOException e) {
-            System.out.println("Файл не найден.");
         }
 
-        return null;
+        return linesList;
     }
 }
