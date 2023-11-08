fce411598e5727c6d2dd5271bb58309ad1846ad7
Julia <u.korjova@gmail.com>
2023-11-08 16:17:43 +0700

    Доб. искл FileNotFoundException e

--- CSV/src/ru/academits/shaduro/csv/main/Main.java
+++ CSV/src/ru/academits/shaduro/csv/main/Main.java
@@ -3,14 +3,17 @@ package ru.academits.shaduro.csv.main;
 
 import ru.academits.shaduro.csv.Csv;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Csv.convertCsvToHtml("input.csv", "output.html");
-        } catch (IOException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("Файл не найден.");
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода/вывода.");
         }
     }
 }
