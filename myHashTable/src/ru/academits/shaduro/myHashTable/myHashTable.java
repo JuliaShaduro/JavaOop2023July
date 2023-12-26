39c75d9755fc7460d27cbe292f758f3f69e1eec0
Julia <u.korjova@gmail.com>
2023-12-26 16:04:04 +0700

    add project HashTable

--- myHashTable/src/ru/academits/shaduro/myHashTable/myHashTable.java
+++ myHashTable/src/ru/academits/shaduro/myHashTable/myHashTable.java
@@ -1,104 +1,253 @@
 package ru.academits.shaduro.myHashTable;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.IntFunction;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.*;
 
-public class myHashTable <T> implements Collection<T> {
+public class myHashTable<T> implements Collection<T> {
+    private final ArrayList<T>[] hashTable;
+    int size;
+    int modCount;
+    private static final int DEFAULT_CAPACITY = 16;
 
-    public myHashTable () {
+    public myHashTable() {
+        //noinspection unchecked
+        hashTable = (ArrayList<T>[]) new ArrayList[DEFAULT_CAPACITY];
+    }
+
+    public myHashTable(int initialCapacity) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Переданное значение capacity = " + initialCapacity + " , должно быть > 0");
+        }
+
+        //noinspection unchecked
+        hashTable = (ArrayList<T>[]) new ArrayList[initialCapacity];
+    }
+
+    private class MyListIterator implements Iterator<T> {  // для какой цели в этой задачи итератор
+        private int currentIndex = -1;
+        private final int startModCount = modCount;
+        private int indexInArray = 0;
+
+        public boolean hasNext() {
+            return currentIndex + 1 < hashTable.length;
+        }
+
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Коллекция закончилась.");
+            }
+
+            if (startModCount != modCount) {
+                throw new ConcurrentModificationException("Коллекция была одновременно изменена другим потоком.");
+            }
+
+            currentIndex++;
+
+            while (hashTable[currentIndex] != null) {
+                if (indexInArray < hashTable[currentIndex].size()) {
+                    T data = hashTable[currentIndex].get(indexInArray);
+                    currentIndex--;
+                    indexInArray++;
+
+                    return data;
+                }
+
+                indexInArray = 0;
+                currentIndex++;
+            }
+
+            return null;
+        }
     }
 
     @Override
-    public <T1> T1[] toArray (IntFunction<T1[]> generator) {
-        return Collection.super.toArray(generator);
+    public boolean contains(Object o) {
+        int index = getIndex(o);
+
+        if (index >= hashTable.length || hashTable[index] == null) {
+            return false;
+        }
+
+        return hashTable[index].contains(o);
     }
 
     @Override
-    public boolean removeIf (Predicate<? super T> filter) {
-        return Collection.super.removeIf(filter);
+    public boolean containsAll(Collection<?> c) {
+        if (c == null) {
+            throw new NullPointerException("Передаваемая коллекция null.");
+        }
+
+        for (Object e : c) {
+            if (!contains(e)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
-    public Spliterator<T> spliterator () {
-        return Collection.super.spliterator();
-    }
+    public boolean remove(Object o) {
+        int index = getIndex(o);
 
-    @Override
-    public Stream<T> stream () {
-        return Collection.super.stream();
-    }
+        if (index >= hashTable.length || hashTable[index] == null) {
+            return false;
+        }
 
-    @Override
-    public Stream<T> parallelStream () {
-        return Collection.super.parallelStream();
-    }
+        if (hashTable[index].remove(o)) {
+            size--;
+            modCount++;
+            return true;
+        }
 
-    @Override
-    public int size () {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty () {
         return false;
     }
 
     @Override
-    public boolean contains (Object o) {
-        return false;
+    public boolean removeAll(Collection<?> c) {
+        if (c == null) {
+            throw new NullPointerException("Передаваемая коллекция null.");
+        }
+
+        for (Object e : c) {
+            if (!remove(e)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
-    public Iterator<T> iterator () {
-        return null;
+    public boolean add(T t) {
+        int index = getIndex(t);
+        //Todo если индекс больше initialCapacity.
+        // Создаем новый ArrayList<T>[] - копируем данные - item ссылаем на новый Array? Такие ситуации редки?  задавать Capacity больше 100? Все равно может не хватить
+//        if (index >= items.length) {
+//            Arrays.copyOf(items,index+1);
+//        }
+
+        if (hashTable[index] == null) {
+            hashTable[index] = new ArrayList<>();
+        }
+
+        hashTable[index].add(t);
+
+        ++size;   //Todo это кол-во элементов countData?
+        ++modCount;
+
+        return true;
     }
 
     @Override
-    public Object[] toArray () {
-        return new Object[0];
+    public boolean addAll(Collection<? extends T> c) {
+        if (c == null) {
+            throw new NullPointerException("Передаваемая коллекция null.");
+        }
+
+        for (T t : c) {
+            if (!add(t)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int getIndex(Object o) {
+        return Math.abs((o == null) ? 0 : o.hashCode() % hashTable.length);
     }
 
     @Override
-    public <T1> T1[] toArray (T1[] a) {
-        return null;
+    public String toString() {
+        return ("myHashTable: {" + Arrays.asList(hashTable) + "}");
     }
 
     @Override
-    public boolean add (T t) {
-        return false;
+    public boolean retainAll(Collection<?> c) {
+        if (c == null) {
+            throw new NullPointerException("Передаваемая коллекция null.");
+        }
+
+        boolean isDeleted = false;
+
+        for (Object e : c) {
+            if (!contains(e)) {
+                remove(e);
+                isDeleted = true;
+            }
+        }
+
+        return isDeleted;
     }
 
     @Override
-    public boolean remove (Object o) {
-        return false;
+    public void clear() {
+        if (size == 0) {
+            return;
+        }
+
+        int i = 0;
+
+        while (i < hashTable.length) {
+            if (hashTable[i] != null) {
+                hashTable[i].clear();
+            }
+
+            i++;
+        }
+
+        size = 0;
+        modCount++;
     }
 
     @Override
-    public boolean containsAll (Collection<?> c) {
-        return false;
+    public int size() {
+        return size;
     }
 
     @Override
-    public boolean addAll (Collection<? extends T> c) {
-        return false;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     @Override
-    public boolean removeAll (Collection<?> c) {
-        return false;
+    public Iterator<T> iterator() {
+        return new MyListIterator();
     }
 
     @Override
-    public boolean retainAll (Collection<?> c) {
-        return false;
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+
+        int i = 0;
+
+        for (ArrayList<T> element : hashTable) {
+            if (element == null) { // или null тоже должны быть ?
+                continue;
+            }
+
+            for (T data : element) {
+                array[i] = data;
+                ++i;
+            }
+        }
+
+        return array;
     }
 
     @Override
-    public void clear () {
+    public <T1> T1[] toArray(T1[] a) {
+        if (a.length < size) {
+            //noinspection unchecked
+            return (T1[]) Arrays.copyOf(toArray(), size);
+        }
 
+        System.arraycopy(toArray(), 0, a, 0, size);
+
+        if (a.length > size) {
+            a[size] = null;
+        }
+
+        return a;
     }
 }
